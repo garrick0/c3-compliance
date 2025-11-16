dbb152975453c8a864edd97290aa61ac644ded26
garrick0 <samuel.gleeson95@gmail.com>
2025-11-16 13:13:50 -0800

    test: add basic sanity test

---
+++ tests/sanity.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+
+describe('c3-compliance package sanity tests', () => {
+  it('should be importable', () => {
+    expect(true).toBe(true);
+  });
+});
+
